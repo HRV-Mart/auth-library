5b59796c5edacf4476c21e663681e11f30768f18
Harsh Verma <55652117+Harsh3305@users.noreply.github.com>
2023-06-18 10:30:25 +0530

    Add auth repository (#8)
    * dded API-Call library
    
    * Adedd auth server url from env
    
    * Created auth repository

---
+++ src/main/kotlin/com/hrv/mart/authlibrary/repository/AuthRepository.kt
@@ -0,0 +1,29 @@
+package com.hrv.mart.authlibrary.repository
+
+import com.hrv.mart.apicall.APICaller
+import com.hrv.mart.authlibrary.model.Auth
+import com.hrv.mart.authlibrary.model.AuthRequest
+import com.hrv.mart.authlibrary.model.UserType
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.http.server.reactive.ServerHttpResponse
+import org.springframework.stereotype.Repository
+import org.springframework.web.reactive.function.client.WebClient
+
+@Repository
+class AuthRepository(
+    @Value("\${hrv.mart.auth.url}")
+    private val authServerUrl: String
+)
+{
+    private val webClientBuilder = WebClient.builder()
+    private val apiCall = APICaller(webClientBuilder)
+
+    fun createAdminAuth(jwt: String, response: ServerHttpResponse) =
+        createAuth(AuthRequest(jwt, UserType.ADMIN), response)
+    fun createUserAuth(jwt: String, response: ServerHttpResponse) =
+        createAuth(AuthRequest(jwt, UserType.USER), response)
+    private fun createAuth(authRequest: AuthRequest, response: ServerHttpResponse) =
+        apiCall
+            .postRequest(authServerUrl, Auth::class.java, authRequest, response)
+
+}
