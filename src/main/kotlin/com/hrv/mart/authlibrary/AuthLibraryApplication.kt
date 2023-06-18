7017eb4a56ddfe3aa5610482149d0a189e840b38
Harsh3305 <harshverma3305@gmail.com>
2023-06-18 08:01:55 +0530

    First commit

---
+++ src/main/kotlin/com/hrv/mart/authlibrary/AuthLibraryApplication.kt
@@ -0,0 +1,11 @@
+package com.hrv.mart.authlibrary
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AuthLibraryApplication
+
+fun main(args: Array<String>) {
+	runApplication<AuthLibraryApplication>(*args)
+}
---
+++ src/test/kotlin/com/hrv/mart/authlibrary/AuthLibraryApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.hrv.mart.authlibrary
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AuthLibraryApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
